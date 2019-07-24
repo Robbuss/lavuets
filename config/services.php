ee6a1cc6380dbf2dd6425b29e4f31cfaeddf526f
Robbuss <vincentvanmontfoort@gmail.com>
2019-07-24 12:51:57 +0200

    fix typo in services

--- config/services.php
+++ config/services.php
@@ -23,7 +23,7 @@ return [
     'passport' => [
       'login_endpoint' => env('PASSPORT_LOGIN_ENDPOINT'),  
       'client_id' => env('PASSPORT_CLIENT_ID'),  
-      'client_secret' => env('PASSPORT_CLiENT_SECRET'),  
+      'client_secret' => env('PASSPORT_CLIENT_SECRET'),  
     ],
 
     'postmark' => [
