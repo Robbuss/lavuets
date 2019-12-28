c39e42ab35997862234459b05a4f8fdc7291b4d7
Robbuss <vincentvanmontfoort@gmail.com>
2019-12-28 18:31:42 +0100

    enable cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -20,7 +20,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\SubDomainMiddleware::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
-        \Spatie\Cors\Cors::class,
+        \App\Http\Middleware\Cors::class,
     ];
 
     /**
